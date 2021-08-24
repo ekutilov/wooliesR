047cbacfed0f2d7bbaf3af0b2ead1fbc009764c2
EKutilov <71804988+ekutilov@users.noreply.github.com>
2021-08-24 20:07:33 +1000

    One-click copy
    JS Downloader script is rewritten so it provides only one json file to copy and paste (that includes both list or receipts and detailed ereceipts themselves).

--- downloader.js
+++ downloader.js
@@ -1,8 +1,8 @@
-var file = []; var file2 = []; var i = 0; 
+var file = []; var file2 = []; var i = 0;
 console.log('Downloading list of transactions...');
 
 do {
-response = $.ajax({ url: "https://api.woolworthsrewards.com.au/wx/v1/rewards/member/ereceipts/transactions/list?page=" + ++i, 
+response = $.ajax({ url: "https://api.woolworthsrewards.com.au/wx/v1/rewards/member/ereceipts/transactions/list?page=" + ++i,
   beforeSend: function(xhr){xhr.setRequestHeader('client_id' , defaultClientId);
                             xhr.setRequestHeader('authorization', "Bearer " + JSON.parse(sessionStorage.authStatusData).access_token);},
   method : 'GET', contentType : 'json', async : false}).responseJSON;
@@ -14,7 +14,7 @@ receiptsList = file.map(function(x) { return x.receiptKey ; });
 console.log('Downloading eReceipts');
 for (let j = 0; j < receiptsList.length; j++) {
 if (receiptsList[j]!==''){
-response2 = $.ajax({ url: "https://api.woolworthsrewards.com.au/wx/v1/rewards/member/ereceipts/transactions/details", 
+response2 = $.ajax({ url: "https://api.woolworthsrewards.com.au/wx/v1/rewards/member/ereceipts/transactions/details",
   beforeSend: function(xhr){xhr.setRequestHeader('client_id' , defaultClientId);
                             xhr.setRequestHeader('authorization' , "Bearer " + JSON.parse(sessionStorage.authStatusData).access_token);
                             xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');},
@@ -24,13 +24,10 @@ response2 = $.ajax({ url: "https://api.woolworthsrewards.com.au/wx/v1/rewards/me
   data : '{"receiptKey" : "' + receiptsList[j] + '" }'
 }).responseJSON;
 response2.data.receiptDetails.key = receiptsList[j];
-file2 = file2.concat(response2.data);
+file[j].ereceipt = response2.data;
 }
 console.log(j + '.. ');
 do {;} while (Date.now() % 100 > 20);
 }
 document.head.innerHTML = '';
-document.body.innerHTML = '<br><br>This is your list of transactions (in json): <br>(copy-paste and save to a plain text file)<br><textarea id="List" rows="30" cols="100">' + JSON.stringify(file) + '</textarea><br><br><br>This is your eReceipts (in json): <br>(copy-paste and save into another plain text file for further use)<br><textarea id="eReceipts" rows="30" cols="100">' + JSON.stringify(file2) + '</textarea>';
-
-
-
+document.body.innerHTML = '<br><br>This is your list of transactions and eReceipts (in json): <br>(copy-paste and save to a plain text file)<br><textarea id="List" rows="50" cols="100">' + JSON.stringify(file);
